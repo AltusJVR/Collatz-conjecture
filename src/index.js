c05256d1804fca855686cf3d8cdc55bea7490e56
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2022-02-26 22:49:32 +0200

    Add brackets to functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-import x3n from './x3n';
+import x3n from "./x3n";
 
-const chooseNumberForm = document.querySelector('.choose_number_form');
-const chooseNumber = document.querySelector('.choose_number');
-const outputList = document.querySelector('.output_list');
+const chooseNumberForm = document.querySelector(".choose_number_form");
+const chooseNumber = document.querySelector(".choose_number");
+const outputList = document.querySelector(".output_list");
 
-const checkEvenOrOdd = num => {
+const checkEvenOrOdd = (num) => {
   if (num % 2 === 0) {
-    return 'Even';
+    return "Even";
   } else {
-    return 'Odd';
+    return "Odd";
   }
 };
 
-const dataOutput = async data => {
+const dataOutput = async (data) => {
   const calculatedArray = data.calculatedArray;
   const startingNumber = data.startingNumber;
   const arrayLength = calculatedArray.length;
-  const seedNumber = document.querySelector('.starting_number');
-  const arrayL = document.querySelector('.array_length');
+  const seedNumber = document.querySelector(".starting_number");
+  const arrayL = document.querySelector(".array_length");
 
   seedNumber.innerHTML = `Seed Number: ${startingNumber}`;
   arrayL.innerHTML = `No of iterations: ${arrayLength}`;
-  calculatedArray.forEach(calc => {
-    let li = document.createElement('li');
+  calculatedArray.forEach((calc) => {
+    let li = document.createElement("li");
     li.innerText = `${checkEvenOrOdd(calc)}: ${calc}`;
     outputList.append(li);
   });
 };
 
 const getNumber = async () => {
-  chooseNumberForm.addEventListener('submit', async e => {
+  chooseNumberForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     if (outputList.hasChildNodes) {
-      outputList.innerHTML = '';
+      outputList.innerHTML = "";
     } else {
       return;
     }
